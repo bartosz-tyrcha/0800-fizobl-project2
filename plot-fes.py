c9af8b664e38f2d4a965c493266709351397c00e
Bartosz Tyrcha <296630@stud.umk.pl>
2021-06-13 13:14:15 +0200

    FES well tempered

--- plot-fes.py
+++ plot-fes.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 T = 300  # K
 k = 1.380649e-26  # kJ/K
-
-a = 4.141947e-24  # [a * kT] = [kJ]
 N_A = 6.02214076e23  # avogadro number
 
+
+# Plot FES form MetaD
 d1_all = []
 free_all = []
 
@@ -23,10 +23,36 @@ free_min = np.min(free_all[-1])
 
 for i in range(10):
     free_all[i] = (free_all[i] - free_min) / (k * T * N_A)
-    plt.plot(d1_all[i], free_all[i], label="{0} ns".format(i))
+    plt.plot(d1_all[i], free_all[i], label="{0} ns".format(i + 1))
 
 plt.xlabel("Odległość Na-Cl [nm]")
 plt.ylabel("Energia swobodna [kT]")
 plt.legend()
-plt.savefig("fes_metad.png")
+plt.savefig("fes_metad.png", dpi=200)
+plt.close()
+
+
+# Plot FES after reweighting
+file_name = "irtg-school-mainz-2020-metad/histo.dat"
+dist, hh, dhh = np.loadtxt(file_name, unpack=True)
+
+free_wt = -np.log(hh)  # in kT
+free_wt = free_wt - np.min(free_wt)
+
+plt.figure()
+plt.plot(dist, free_wt, "olivedrab")
+plt.xlabel("Odległość Na-Cl [nm]")
+plt.ylabel("Energia swobodna [kT]")
+plt.savefig("fes_welltempered.png", dpi=200)
+plt.close()
+
+
+# Plot comparison
+plt.figure()
+plt.plot(d1_all[-1], free_all[-1], "r", label='Przed "ważeniem"')
+plt.plot(dist, free_wt, "olivedrab", label='Po "ważeniu"')
+plt.xlabel("Odległość Na-Cl [nm]")
+plt.ylabel("Energia swobodna [kT]")
+plt.legend()
+plt.savefig("fes_comparison.png", dpi=200)
 plt.close()
